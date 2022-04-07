a5a902c795a2dfa1bd0031657c119bb564e8794c
ArthurCDourado <arthurdourado01@hotmail.com>
2022-04-07 17:55:14 -0300

    conclusão do módulo de fundamentos

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,16 @@ import DiretaPai from "./components/comunicacao/DiretaPai";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
 import Input from "./components/formulario/Input";
 import Contador from "./components/contador/Contador";
+import Mega from "./components/mega/Mega";
 
 export default _ =>
     <div className="App">
         <h1>Fundamentos React (Arrow)</h1>
 
         <div className="Cards">
+            <Card titulo="#13 - Mega" color="#333">
+                <Mega qtde={8}/>
+            </Card>
             <Card titulo="#12 - Contador" color="#3d1e6d">
                 <Contador numeroInicial={20}/>
             </Card>
